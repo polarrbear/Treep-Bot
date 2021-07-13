9501ae1c46288f572af9fda6e02dacea5f7c9303
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-14 05:28:29 +0700

    Update spam.py

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -10,7 +10,7 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.cspam (.*)")
+@register(outgoing=True, pattern="^\\.cspam (.+)")
 async def tmeme(e):
     cspam = str(e.pattern_match.group(1))
     message = cspam.replace(" ", "")
@@ -19,12 +19,12 @@ async def tmeme(e):
         await e.respond(letter)
     if BOTLOG:
         await e.client.send_message(
-            BOTLOG_CHATID, "#CSPAM\n"
-            "TSpam was executed successfully")
+            BOTLOG_CHATID, "#CSPAM\n" "TSpam was executed successfully"
+        )
 
 
-@register(outgoing=True, pattern="^.wspam (.*)")
-async def tmeme(e):
+@register(outgoing=True, pattern=r"^\.wspam (.+)")
+async def t_meme(e):
     wspam = str(e.pattern_match.group(1))
     message = wspam.split()
     await e.delete()
@@ -32,62 +32,64 @@ async def tmeme(e):
         await e.respond(word)
     if BOTLOG:
         await e.client.send_message(
-            BOTLOG_CHATID, "#WSPAM\n"
-            "WSpam was executed successfully")
+            BOTLOG_CHATID, "#WSPAM\n" "WSpam was executed successfully"
+        )
 
 
-@register(outgoing=True, pattern="^.spam (.*)")
+@register(outgoing=True, pattern=r"^\.spam (\d+) (.+)")
 async def spammer(e):
-    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
-    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
+    counter = int(e.pattern_match.group(1))
+    spam_message = str(e.pattern_match.group(2))
     await e.delete()
     await asyncio.wait([e.respond(spam_message) for i in range(counter)])
     if BOTLOG:
-        await e.client.send_message(BOTLOG_CHATID, "#SPAM\n"
-                                    "Spam was executed successfully")
+        await e.client.send_message(
+            BOTLOG_CHATID, "#SPAM\n" "Spam was executed successfully"
+        )
 
 
-@register(outgoing=True, pattern="^.picspam")
+@register(outgoing=True, pattern=r"^\.picspam (\d+) (.+)")
 async def tiny_pic_spam(e):
-    message = e.text
-    text = message.split()
-    counter = int(text[1])
-    link = str(text[2])
+    counter = int(e.pattern_match.group(1))
+    link = str(e.pattern_match.group(2))
     await e.delete()
     for _ in range(1, counter):
         await e.client.send_file(e.chat_id, link)
     if BOTLOG:
         await e.client.send_message(
-            BOTLOG_CHATID, "#PICSPAM\n"
-            "PicSpam was executed successfully")
+            BOTLOG_CHATID, "#PICSPAM\n" "PicSpam was executed successfully"
+        )
 
 
-@register(outgoing=True, pattern="^.supermen (.*)")
+@register(outgoing=True, pattern="^.delayspam (.*)")
 async def spammer(e):
-    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
-    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
-    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
+    spamDelay = float(e.pattern_match.group(1).split(" ", 2)[0])
+    counter = int(e.pattern_match.group(1).split(" ", 2)[1])
+    spam_message = str(e.pattern_match.group(1).split(" ", 2)[2])
     await e.delete()
     for _ in range(1, counter):
         await e.respond(spam_message)
         await sleep(spamDelay)
     if BOTLOG:
         await e.client.send_message(
-            BOTLOG_CHATID, "#DelaySPAM\n"
-            "DelaySpam was executed successfully")
+            BOTLOG_CHATID, "#DelaySPAM\n" "DelaySpam was executed successfully"
+        )
 
 
-CMD_HELP.update({
-    "spam":
-    "`.cspam` <text>\
-\nUsage: Spam the text letter by letter.\
-\n\n`.spam` <count> <text>\
-\nUsage: Floods text in the chat !!\
-\n\n`.wspam` <text>\
-\nUsage: Spam the text word by word.\
-\n\n`.picspam` <count> <link to image/gif>\
-\nUsage: As if text spam was not enough !!\
-\n\n`.supermen` <delay> <count> <text>\
-\nUsage: `.bigspam` but with custom delay.\
-\n\n\nNOTE : Spam at your own risk !!"
-})
+CMD_HELP.update(
+    {
+        "spam": "**Plugin : **`spam`\
+        \n\n  •  **Syntax :** `.spam` <jumlah spam> <text>\
+        \n  •  **Function : **Membanjiri teks dalam obrolan!! \
+        \n\n  •  **Syntax :** `.cspam` <text>\
+        \n  •  **Function : **Spam surat teks dengan huruf. \
+        \n\n  •  **Syntax :** `.wspam` <text>\
+        \n  •  **Function : **Spam kata teks demi kata. \
+        \n\n  •  **Syntax :** `.picspam` <jumlah spam> <link image/gif>\
+        \n  •  **Function : **Spam Foto Seolah-olah spam teks tidak cukup !! \
+        \n\n  •  **Syntax :** `.delayspam` <detik> <jumlah spam> <text>\
+        \n  •  **Function : **Spam surat teks dengan huruf. \
+        \n\n  •  **NOTE : Spam dengan Risiko Anda sendiri**\
+    "
+    }
+)
